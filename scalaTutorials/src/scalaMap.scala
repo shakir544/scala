36d07e9b3d45928563e86b0ae6265481967a2c54
Shakir Gooty <shakir544@gmail.com>
2016-03-14 23:59:13 -0400

    Map Example in scala

---
+++ scalaTutorials/src/scalaMap.scala
@@ -0,0 +1,21 @@
+
+
+object scalaMap {
+  
+  def main(args :Array[String]) { 
+    
+    // Map example 
+    
+    val x = List(1,2,3,4) 
+    
+    val ys = x.map { x => x *100 }
+    
+    ys.foreach { println }
+    
+
+   
+    
+    
+  }
+  
+}
